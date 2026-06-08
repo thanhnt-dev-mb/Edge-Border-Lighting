4169375a5d9fe6b40093dadc79915c2f3f4e33f9
chinhnv <nguyenchinh992000@gmail.com>
2026-06-08 15:12:53 +0700

    fix: migrate toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,11 @@ pluginManagement {
 
     includeBuild("build-logic")
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
