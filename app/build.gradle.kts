5bb4ea9eb38bdd130c41c0533df23c31193605b6
dungvuhanoivietnam <108311932+dungvuhanoivietnam@users.noreply.github.com>
2026-06-16 15:35:12 +0700

    V6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.edge.lighting.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
